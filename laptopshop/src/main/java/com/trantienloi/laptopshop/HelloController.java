62790e6ca78b59c4eff41ebcdc1c58f5e6751624
trantienloi <trantienloi412@gmail.com>
2024-10-23 17:28:26 +0700

    Add dependencies

--- laptopshop/src/main/java/com/trantienloi/laptopshop/HelloController.java
+++ laptopshop/src/main/java/com/trantienloi/laptopshop/HelloController.java
@@ -2,14 +2,21 @@ package com.trantienloi.laptopshop;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 public class HelloController {
     @GetMapping("/")
     public String getMethodName() {
-        return "Laptopshop";
+        return "Hello Word, update1223";
+    }
+    @GetMapping("/user")
+    public String UserPage() {
+        return "Only user can access this page, update";
+    }
+    @GetMapping("/admin")
+    public String AdminPage() {
+        return "Only admin can access this page";
     }
     
     
